987ed8fa2cb05328b9bc4def4ca643630ae14390
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-10-18 15:34:39 +0000

    hurr

---
+++ .gitlab/ci/upload.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: AGPL-3.0
+
+#    ----------------------------------------------------------------------
+#    Copyright © 2022, 2023, 2024, 2025  Pellegrino Prevete
+#
+#    All rights reserved
+#    ----------------------------------------------------------------------
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+# This script is run within a virtual environment to build
+#  pakcage
+# $1: platform
+# $2: architecture
+
+set \
+  -euo \
+    pipefail
+shopt \
+  -s \
+    extglob
+
+_upload() {
+  local \
+    _assets_links=() \
+    _release_cli_create_opts=()
+  pwd
+  ls
+  for _file \
+    in "dogeos-gnu"*".pkg.tar."*; do
+    curl \
+      --silent \
+      --header \
+        "JOB-TOKEN: ${ci_job_token}" \
+      --upload-file \
+        ${_file} \
+      "${package_registry_url}/${_file}"
+    _assets_links+=(
+      --asset-link
+        "'{ \"name\": \"${_file}\", \"url\": \"${package_registry_url}/${_file}\" }'"
+    )
+  done
+  _release_cli_create_opts+=(
+    --name
+      "Release: ${tag}"
+    --tag-name \
+      "${tag}"
+    "${_assets_links[@]}"
+  )
+  release-cli \
+    create \
+    "${_release_cli_create_opts[@]}"
+}
+
+readonly \
+  platform="${1}" \
+  arch="${2}" \
+  ns="${3}" \
+  pkg="${4}" \
+  tag="${5}" \
+  ci_job_token="${6}" \
+  package_registry_url="${7}"
+
+_upload
+
+# vim:set sw=2 sts=-1 et:
