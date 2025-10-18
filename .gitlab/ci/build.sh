ee17f7b11f759a10f28ecc3b4ade930988348d2a
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-10-18 17:14:33 +0000

    hurr

--- .gitlab/ci/build.sh
+++ .gitlab/ci/build.sh
@@ -116,31 +116,6 @@ _requirements() {
     "fur" \
     "${_fur_mini_opts[@]}"
   _fur_release="0.0.1.1.1.1.1.1.1.1.1.1.1"
-  # glab \
-  #   auth \
-  #     login \
-  #       --hostname \
-  #         "gitlab.com" \
-  #       --api-protocol \
-  #         "https" \
-  #       --token \
-  #         "$(cat \
-  #              "/root/.config/gitlab.com/default.txt")"
-  # glab \
-  #   release \
-  #     download \
-  #       "${_fur_release}" \
-  #     -R \
-  #     "themartiancompany/fur-ur"
-  #     --asset-name="*.pkg.tar.xz"
-  # pwd
-  # ls \
-  #   "/root"
-  # cat \
-  #   "/root/dogeos-gnu-fur"*".pkg.tar."*
-  # pacman \
-  #   -Udd \
-  #   "dogeos-gnu-fur"*".pkg.tar."*
   _fur_opts+=(
     -v
     -p
