801c26cd8b1cabf0f63d62fc45270dbd1523b8a7
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-10-18 17:44:28 +0000

    hurra

--- .gitlab/ci/build.sh
+++ .gitlab/ci/build.sh
@@ -137,8 +137,6 @@ _requirements() {
     "${ns}" \
     "${_pkgname}" \
     "${_commit}"
-  cat \
-    "${HOME}/${_pkgname}-${_commit}.tar.gz"
   mv \
     "${HOME}/${_pkgname}-${_commit}.tar.gz" \
     "/home/user/${_pkgname}"
